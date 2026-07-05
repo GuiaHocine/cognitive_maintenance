5e7693b107c2f939cfe2a60711914330ecccd795
hocine <guiahocine@gmail.com>
2026-07-05 14:57:53 +0200

    basic_heuristics_implementations

--- utils.py
+++ utils.py
@@ -7,21 +7,18 @@ def MSE(y_pred:np.ndarray,y_true:np.ndarray)->float:
     return np.average((error**2),axis=0)
 
 
-def cross_entropy_loss(y_pred:np.ndarray,y_true:np.ndarray)->float:
+def cross_entropy_loss(y_pred:np.ndarray,y_true:np.ndarray,option:int = 1)->float:
     """
     y_pred (B,nbr_classes)
     y_true(B,nbr_classes)
     """
-    log_y_hat = np.log(y_pred) # natural logarithmic for efficiency (nats)
-    y_hat = np.clip(log_y_hat,1e-15 , 1.0 - 1e-15)
 
-
-    """ Option 1 : 
+    """ Option 2 : Computationally inefficient 
     computations = (-y_true.T) @ y_hat # (B,B)   # need to create a big square matrix to extract the diag!
     losses = np.diag(computations) # (B,1)
     """
 
-    """ option 2 : optimized for one hot encoding : y_true  of shape (B,)
+    """ option 3 : optimized for one hot encoding : y_true  of shape (B,)
 
     B = y_pred.shape[0]
     losses = - np.log(y_pred[np.arange(B),y_true]) # (B,1)
@@ -29,9 +26,48 @@ def cross_entropy_loss(y_pred:np.ndarray,y_true:np.ndarray)->float:
     
     """
 
-    # general_case 
-    computations = (-y_true.T)*y_hat # (B,nbr_classes) --> element wise multiplication
-    losses = np.sum(computations,axis = 1) # (B,1)
-    
-    return np.average(losses,axis = 0)
+    log_y_hat = np.log(y_pred) # natural logarithmic for efficiency (nats)
+    y_hat = np.clip(log_y_hat,1e-15 , 1.0 - 1e-15)
 
+    if option == 1: 
+        computations = (-y_true.T)*y_hat # (B,nbr_classes) --> element wise multiplication
+        losses = np.sum(computations,axis = 1) # (B,1)
+        
+        return np.average(losses,axis = 0)
+    
+    if option == 2:
+        computations = (-y_true.T) @ y_hat # (B,B)   # need to create a big square matrix to extract the diag!
+        losses = np.diag(computations) # (B,1)
+        return np.average(losses,axis = 0)
+
+    if option == 3:
+        B = y_pred.shape[0]
+        losses = - np.log(y_pred[np.arange(B),y_true]) # (B,1)
+        return np.average(losses,axis = 0)
+
+  
+
+
+def mlp_layer(x:np.ndarray,W:np.ndarray)->np.ndarray:
+    return x @ W # (B,dim) @ (dim,dim_output) -> (B,dim_output)
+
+
+def relu_layer(x:np.ndarray) -> np.ndarray:
+    return (np.max(x,0.0 + 1e-10))
+
+def sigmoid_layer(x:np.ndarray) -> np.ndarray:
+    return (np.exp(x) / np.exp(x) + 1)
+
+def softmax(x:np.ndarray) -> np.ndarray:
+    exp_x = np.exp(x) # (B,DIM ) 
+    sums = 1 / np.sum(exp_x,axis = 1) # (B,)
+    return (sums[:,None] * exp_x)  # (B,1) * (B,DIM) ---> (B,DIM) * (B,DIM) --> (B,DIM)  Virtual Broadcasting
+
+
+
+""""
+
+to do :  keepdims avoid doing doing [:,None]
+      : substracting max for numerical stability
+
+"""
